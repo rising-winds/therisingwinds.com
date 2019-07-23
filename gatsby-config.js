b78f490b6a72cae99eae8fd2878ed11087b41499
Russell Skaggs <russell.skaggs@huntington.com>
2019-07-22 23:47:54 -0400

    Updating For Blog Template

--- gatsby-config.js
+++ gatsby-config.js
@@ -5,5 +5,16 @@
  */
 
 module.exports = {
-  plugins: [`gatsby-plugin-netlify-cms`],
+  
+  plugins: [
+    {
+      resolve: `gatsby-source-filesystem`,
+      options: {
+        name: `markdown-pages`,
+        path: `${__dirname}/blog`,
+      },
+    },
+    `gatsby-plugin-netlify-cms`,
+    `gatsby-transformer-remark`,
+  ],
 }
